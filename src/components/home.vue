2fb2db6b4e6fa6470e73eb19873c8cfb0f902eae
leo <863249346@qq.com>
2020-09-11 12:20:00 +0800

    'login_finish'

---
+++ src/components/home.vue
@@ -0,0 +1,26 @@
+<template>
+ <div>
+   <el-button type="info" @click="handleLogout">退出</el-button>
+ </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    // 退出事件
+    handleLogout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
